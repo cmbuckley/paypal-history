72ce13b25548c6bf4ed5fea63cafc38cbc539b9c
Chris Buckley <chris@cmbuckley.co.uk>
2017-03-21 00:24:15 +0000

    Move parser include logic from provider to test

--- test/Parser/CsvTest.php
+++ test/Parser/CsvTest.php
@@ -9,25 +9,19 @@ class CsvTest extends \PHPUnit_Framework_TestCase {
     /**
      * @dataProvider providerLoadFile
      */
-    public function testLoadFile($csv, $expected) {
+    public function testLoadFile($csv, $php) {
         $fixture = new Csv(array('currency' => 'GBP'));
         $fixture->loadFile($csv);
 
-        $this->assertEquals($expected, $fixture->getData());
+        $this->assertFileExists($php);
+        $this->assertEquals(include $php, $fixture->getData());
     }
 
     public function providerLoadFile() {
         $tests = array();
 
         foreach (glob('test/etc/csv/*.csv') as $file) {
-            $phpFile = str_replace('csv', 'php', $file);
-            $expected = include $phpFile;
-
-            if (!$expected) {
-                throw new \Exception("Missing file $phpFile");
-            }
-
-            $tests[basename($file)] = array($file, $expected);
+            $tests[basename($file)] = array($file, str_replace('csv', 'php', $file));
         }
 
         return $tests;
