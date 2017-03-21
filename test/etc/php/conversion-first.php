386ea672624d621c40c4bcbf56f7c87cd80e48e0
Chris Buckley <chris@cmbuckley.co.uk>
2017-03-21 01:22:53 +0000

    Add failing test when conversion lines appear before transaction

---
+++ test/etc/php/conversion-first.php
@@ -0,0 +1,20 @@
+<?php return array(
+    array(
+        'date' => new \DateTime('2017-03-07 12:54:40', new \DateTimeZone('UTC')),
+        'name' => 'Bank Account',
+        'type' => 'Add Money from a Bank Account',
+        'currency' => 'GBP',
+        'rate' => 1,
+        'amount' => 1093,
+        'id' => '',
+    ),
+    array(
+        'date' => new \DateTime('2017-03-07 12:54:40', new \DateTimeZone('UTC')),
+        'name' => 'Some Store',
+        'type' => 'Mobile Express Checkout Payment Sent',
+        'currency' => 'USD',
+        'rate' => 1093 / 1290,
+        'amount' => -1290,
+        'id' => '',
+    ),
+);
